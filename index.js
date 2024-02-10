f901a4db1d028a71711213a3ee8844c2cd77feb4
Manoranjan Sahoo <manoranjanheremano@gmail.com>
2024-02-10 13:09:18 +0530

    ejs added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 console.dir(app)
 let port =8080;
 app.listen(port, ()=>
@@ -8,7 +9,7 @@ app.listen(port, ()=>
 })
 app.get("/",(req,res)=>
 {
-    res.send("you contacted root path")
+    res.render("home.ejs")
 })
 app.get("/home",(req,res)=>
 {
@@ -30,8 +31,8 @@ app.get("/:username/:id", (req, res) => {
 }) 
 // app.use((req,res)=>{
 //     console.log("request receivied")
-    
 //     res.send("this is a basic response")
 // })
-
-//we use express for req listening, req parse to object, req route, and send suitable response.
+//we use express for req listening, req parse from hhtp text to object, req route, and send suitable response.
+app.set("view engine", "ejs")
+app.set("views",path.join(__dirname,"/views"));
